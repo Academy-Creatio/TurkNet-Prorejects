23ed24de401996581a6fb96a177b5336c39840f8
Kirill Krylov <k.krylov@creatio.com>
2022-04-08 12:10:15 +0300

    more UI

--- TurkNet/Schemas/ContactPageV2/ContactPageV2.js
+++ TurkNet/Schemas/ContactPageV2/ContactPageV2.js
@@ -1,6 +1,16 @@
 define("ContactPageV2", [], function() {
 	return {
 		entitySchemaName: "Contact",
+		messages:{
+			/**
+			 * Published on: ContactSectionV2
+			 * @tutorial https://academy.creatio.com/docs/developer/front-end_development/sandbox_component/module_message_exchange
+			 */
+			"SectionActionClicked":{
+				mode: this.Terrasoft.MessageMode.PTP,
+				direction: this.Terrasoft.MessageDirectionType.SUBSCRIBE
+			}
+		},
 		attributes: {
 
 			"Account": {
@@ -25,27 +35,48 @@ define("ContactPageV2", [], function() {
 		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
 		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
 		methods: {
-
+			//#region PAGE LIFECYCLE
 			/**
 			* @inheritdoc Terrasoft.BasePageV2#init
 			*/
 			init: function() {
 				this.callParent(arguments);
+				this.subscribeToMessages();
 			},
-
 			/**
 			* @inheritdoc Terrasoft.BasePageV2#onEntityInitialized
 			*/
 			onEntityInitialized: function() {
 				this.callParent(arguments);
 			},
+			//#endregion
 
+			//#region MESSAGES
+			subscribeToMessages: function(){
+				this.sandbox.subscribe(
+					"SectionActionClicked",
+					function(args){this.onSectionMessageReceived(args);},
+					this,
+					[this.sandbox.id]
+				)
+			},
+			onSectionMessageReceived: function(args){
+				let arg1 = args.arg1;
+				let arg2 = args.arg2;
+				this.showInformationDialog("Message received with arguments+: arg1: "+arg1+" arg2: "+arg2);
+			},
+			//#endregion
+
+			//#region EVENTS-ATTRIBUTES
 			onNameChanged: function(){
 				this.showInformationDialog("Name has changed to: "+this.$Name);
 			},
 			onEmailChanged: function(){
 				//this.showInformationDialog("Email has changed to: "+this.$Email);
 			},
+			//#endregion
+
+			//#region VALIDATION
 			
 			/**
 			 * @inheritdoc Terrasoft.BaseSchemaViewModel#setValidationConfig
@@ -72,6 +103,7 @@ define("ContactPageV2", [], function() {
 					invalidMessage: invalidMessage
 				};
 			 },
+
 			 nameValidator: function(){
 				var invalidMessage = "";
 				
@@ -85,10 +117,71 @@ define("ContactPageV2", [], function() {
 				return {
 					invalidMessage: invalidMessage
 				};
-			 }
+			 },
+			//#endregion
 
-
-
+			//#region ACTIONS
+			/**
+			 * @inheritdoc Terrasoft.BasePageV2#getActions
+			 * @overridden
+			 */
+			getActions: function() {
+				var actionMenuItems = this.callParent(arguments);
+				actionMenuItems.addItem(this.getButtonMenuSeparator());
+				actionMenuItems.addItem(this.getButtonMenuItem({
+					"Tag": "tag1",
+					//"Caption": {"bindTo": "Resources.Strings.ActionItemOne"},
+					"Caption":  this.get("Resources.Strings.ActionItemOne"),
+					"Click": {"bindTo": "onActionItemClicked"},
+					ImageConfig: this.get("Resources.Images.CreatioSquare")
+				}));
+				actionMenuItems.addItem(this.getButtonMenuItem({
+					"Tag": "tag2",
+					"Caption": this.get("Resources.Strings.ActionItemTwo"),
+					"Click": {"bindTo": "onActionItemClicked"},
+					Items: this.addSubItems(),
+					ImageConfig: this.get("Resources.Images.CreatioSquare")
+				}));
+				return actionMenuItems;
+			},
+			addSubItems: function(){
+				var collection = this.Ext.create("Terrasoft.BaseViewModelCollection");
+				collection.addItem(this.getButtonMenuItem({
+					"Caption": this.get("Resources.Strings.SubActionOneCaption"),
+					"Click": {"bindTo": "onSubActionOneClick"},
+					"Tag": "sub1"
+				}));
+				collection.addItem(this.getButtonMenuItem({
+					"Caption": this.get("Resources.Strings.SubActionTwoCaption"),
+					"Click": {"bindTo": "onSubActionTwoClick"},
+					"Tag": "sub2"
+				}));
+				return collection;
+			},
+			onActionItemClicked: function(){
+				let tag = arguments[0];
+				this.showInformationDialog("onActionItemClicked");
+			},
+			onSubActionOneClick: function(){
+				let tag = arguments[0];
+				this.showInformationDialog("onSubActionOneClick");
+			},
+			onSubActionTwoClick: function(){
+				let tag = arguments[0];
+				this.showInformationDialog("onSubActionTwoClick");
+			},
+			//#endregion
+			
+			//#region BUTTON HANDLERS
+			
+			/**
+			 * Handles MyRedButton click
+			 */
+			 onMyRedButtonClick: function(){
+				let tag = arguments[3];
+				this.showInformationDialog("MyRedButton Clicked, tag is: "+tag);
+			}
+			//#endregion
 		},
 		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
 		diff: /**SCHEMA_DIFF*/[
@@ -108,7 +201,32 @@ define("ContactPageV2", [], function() {
 				"parentName": "ContactGeneralInfoBlock",
 				"propertyName": "items",
 				"index": 6
-			}
+			},
+			{
+				"operation": "insert",
+				"name": "MyRedButton",
+				"values": {
+					"itemType": 5,
+					"style": "red",
+					"classes": {
+						"textClass": [
+							"actions-button-margin-right"
+						],
+						"wrapperClass": [
+							"actions-button-margin-right"
+						]
+					},
+					"caption": "Red button",
+					"hint": "Page red button hint",
+					"click": {
+						"bindTo": "onMyRedButtonClick"
+					},
+					"tag": "LeftContainer_Red"
+				},
+				"parentName": "LeftContainer",
+				"propertyName": "items",
+				"index": 7
+			},
 		]/**SCHEMA_DIFF*/
 	};
 });
--- TurkNet/Schemas/ContactSectionV2/ContactSectionV2.js
+++ TurkNet/Schemas/ContactSectionV2/ContactSectionV2.js
@@ -1,8 +1,72 @@
 define("ContactSectionV2", [], function() {
 	return {
 		entitySchemaName: "Contact",
+		messages:{
+			/**
+			 * Subscribed on: ContactPageV2 
+			 * @tutorial https://academy.creatio.com/docs/developer/front-end_development/sandbox_component/module_message_exchange
+			 */
+			"SectionActionClicked":{
+				mode: this.Terrasoft.MessageMode.PTP,
+				direction: this.Terrasoft.MessageDirectionType.PUBLISH
+			}
+		},
 		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
-		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
-		methods: {}
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "PrimaryContactButtonRed",
+				"parentName": "CombinedModeActionButtonsCardLeftContainer", //visible on the  page, INVISIBLE in the section, 
+				"propertyName": "items",
+				"values":{
+					itemType: this.Terrasoft.ViewItemType.BUTTON,
+					style: Terrasoft.controls.ButtonEnums.style.RED,
+					classes: {
+						"textClass": ["actions-button-margin-right"],
+						"wrapperClass": ["actions-button-margin-right"]
+					},
+					caption: "Red Button",
+					hint: "Section red button hint",
+					click: {"bindTo": "onMyMainButtonClick"},
+					tag: "CombinedModeActionButtonsCardLeftContainer_Red"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "PrimaryContactButtonGreen",
+				"parentName": "SeparateModeActionButtonsLeftContainer", //visible in section and INVISIBLE on the page
+				"propertyName": "items",
+				"values":{
+					itemType: this.Terrasoft.ViewItemType.BUTTON,
+					style: Terrasoft.controls.ButtonEnums.style.GREEN,
+					classes: {
+						textClass: ["actions-button-margin-right"],
+						wrapperClass: ["actions-button-margin-right"]
+					},
+					caption: "Green Button",
+					hint: "Section red button hint",
+					click: {"bindTo": "onMyMainButtonClick"},
+					tag: "ActionButtonsContainer_Red"
+				}
+			}
+		]/**SCHEMA_DIFF*/,
+		methods: {
+
+			onMyMainButtonClick: function(a, b, c, tag){
+
+				//var primaryColumnValue = this.$ActiveRow;
+				//var gridData = this.getGridData();
+				//var activeRow = gridData.get(primaryColumnValue);
+				
+				//SEND THE MESSAGE - Message publication without a handler method argument.
+				this.sandbox.publish(
+					"SectionActionClicked",
+					{arg1: 5, arg2: "arg2"}, 
+					[this.sandbox.id+"_CardModuleV2"]
+				);
+				
+			}
+
+		}
 	};
 });
